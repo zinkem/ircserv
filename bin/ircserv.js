5e55e98405d3bd83173ad5fa4cef9d4fe3dc98fb
zinkem <zinkem@gmail.com>
2018-09-29 13:51:28 -0700

    modular refactor stage 1

---
+++ bin/ircserv.js
@@ -0,0 +1,26 @@
+'use strict';
+const proc = require('process');
+
+const ircserv = require('../');
+
+const server_config = require('../config/default_server_config.json');
+
+process.on('uncaughtException', (err) => {
+  console.error(err);
+  process.exit(1);
+});
+
+console.log(server_config);
+
+const ircd = new ircserv.Server(server_config);
+ircd.listen();
+
+
+setInterval(() => {
+  const mem_string = `${proc.memoryUsage().rss / 1000000}M`;
+  console.log(`${Object.keys(ircd.users).length} ${mem_string}`);
+}, 30000);
+
+
+
+
